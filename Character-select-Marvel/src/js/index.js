e23f0ebedfbcaadae8069fd13e72c8b0b0d6bf97
João Vitor Alves Fialho <oclubedosdez90@gmail.com>
2022-05-15 16:42:19 -0300

    Character-select-Marvel

---
+++ Character-select-Marvel/src/js/index.js
@@ -0,0 +1,27 @@
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const idSelecionado = personagem.attributes.id.value;
+
+        if (idSelecionado === 'ultron') return;
+        /*OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:*/
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado')
+
+        /*OBJETIVO 2 - qunado passar o mouse em cima do personagem da listagem, troca a imagem e nome do personagem grande*/
+
+       const imagemJogador1 = document.getElementById('personagem-jogador-1')
+       imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
+
+       const nomejogador1 = document.getElementById('nome-jogador-1')
+       const nomeSeleceionado = personagem.getAttribute('data-name');
+
+       nomejogador1.innerHTML = nomeSeleceionado;
+    });
+});
+
+
+
